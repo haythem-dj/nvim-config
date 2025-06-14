716f71b482053ba62c300b5866e856c1ea3829d3
haythem-dj <haythemdjebbar5@gmail.com>
2025-06-14 17:41:15 +0100

    Add buffers and bufferline

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,5 +1,22 @@
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return {
+			desc = "nvim-tree: " .. desc,
+			buffer = bufnr,
+			noremap = true,
+			silent = true,
+			nowait = true,
+		}
+	end
+
+	vim.keymap.set("n", "<leader><CR>", api.node.open.tab, opts("Open in New Tab"))
+end
+
 return {
 	"nvim-tree/nvim-tree.lua",
+	on_attach = my_on_attach,
 	config = function()
 		require("nvim-tree").setup({
 			filters = {
