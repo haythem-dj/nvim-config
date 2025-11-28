5d6d022bd4b69e11629f7520a87bc39284e00bbe
haythem-dj <haythemdjebbar5@gmail.com>
2025-11-28 18:25:43 +0100

    committing

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,29 +1,52 @@
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Line numbers
 vim.opt.nu = true
 vim.opt.rnu = true
 
+-- Tabs and indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- Display
 vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
 
+-- Searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-
+-- Update time
 vim.opt.updatetime = 50
 
+-- Other options
 vim.o.whichwrap = "b,s,<,>,[,],h,l"
 vim.o.clipboard = "unnamedplus"
-
 vim.opt.guicursor = "n-v-c-i:block"
 
-vim.g.mapleader = " "
+-- VimTeX settings
+vim.cmd([[
+  " Enable filetype plugins and indentation
+  filetype plugin indent on
+
+  " Enable syntax highlighting
+  syntax enable
+
+  " Viewer options
+  let g:vimtex_view_method = 'zathura'
+
+  " Alternative generic viewer (optional)
+  let g:vimtex_view_general_viewer = 'okular'
+  let g:vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
+
+  " Compiler method
+  let g:vimtex_compiler_method = 'latexmk'
+]])
